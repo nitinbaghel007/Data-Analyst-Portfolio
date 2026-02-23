bc48b69fb78fc83be0dcc544f2e84ec4e51f3497
Nitin baghel <nitinbaghel272@gmail.com>
2026-02-23 14:08:34 +0530

    Update retail_store_sales_intelligence.sql

--- SQL/Retail_Store_Sales_Intelligence_Project/retail_store_sales_intelligence.sql
+++ SQL/Retail_Store_Sales_Intelligence_Project/retail_store_sales_intelligence.sql
@@ -5,106 +5,106 @@
 
 
 -- 1. Total Records
-SELECT COUNT(*) AS total_transactions
-FROM retail_store_sales_clean;
+select count(*) as total_transactions
+from retail_store_sales_clean;
 
 
 -- 2. Total Revenue
-SELECT SUM(total_spent) AS total_revenue
-FROM retail_store_sales_clean;
+select SUM(total_spent) as total_revenue
+from retail_store_sales_clean;
 
 
 -- 3. Monthly Revenue Trend
-SELECT 
+select 
     year,
     month,
-    SUM(total_spent) AS monthly_revenue
+    sum(total_spent) as monthly_revenue
 FROM retail_store_sales_clean
-GROUP BY year, month
-ORDER BY year, month;
+group by year, month
+order by year, month;
 
 
 -- 4. Running Revenue (Cumulative)
-SELECT 
+select 
     year,
     month,
-    SUM(total_spent) AS monthly_revenue,
-    SUM(SUM(total_spent)) OVER (ORDER BY year, month) AS running_total
-FROM retail_store_sales_clean
-GROUP BY year, month
-ORDER BY year, month;
+    sum(total_spent) as monthly_revenue,
+    sum(sum(total_spent)) over(order by year, month) as running_total
+from retail_store_sales_clean
+group by year, month
+order by year, month;
 
 
 -- 5. Revenue by Category
-SELECT 
+select 
     category,
-    SUM(total_spent) AS revenue
-FROM retail_store_sales_clean
-GROUP BY category
-ORDER BY revenue DESC;
+    sum(total_spent) as revenue
+from retail_store_sales_clean
+group by category
+order by revenue desc;
 
 
 -- 6. Top 3 Revenue Contributing Categories (Pareto Style)
-SELECT 
+select 
     category,
-    SUM(total_spent) AS revenue,
-    ROUND(
-        SUM(total_spent) /
-        (SELECT SUM(total_spent) FROM retail_store_sales_clean) * 100,
+    SUM(total_spent) as revenue,
+    round(
+        sum(total_spent) /
+        (select sum(total_spent) from retail_store_sales_clean) * 100,
         2
-    ) AS contribution_percent
-FROM retail_store_sales_clean
-GROUP BY category
-ORDER BY revenue DESC
-LIMIT 3;
+    ) as contribution_percent
+from retail_store_sales_clean
+group by category
+order by revenue desc
+limit 3;
 
 
 -- 7. Customer Lifetime Value (Top 5 Customers)
-SELECT 
+select 
     customer_id,
-    SUM(total_spent) AS lifetime_value
-FROM retail_store_sales_clean
-GROUP BY customer_id
-ORDER BY lifetime_value DESC
-LIMIT 5;
+    sum(total_spent) as lifetime_value
+from retail_store_sales_clean
+group by customer_id
+order by lifetime_value desc
+limit 5;
 
 
 -- 8. Customer Segmentation (Gold / Silver / Bronze)
-SELECT 
+select 
     customer_id,
-    SUM(total_spent) AS lifetime_value,
-    CASE 
-        WHEN SUM(total_spent) >= 62000 THEN 'Gold'
-        WHEN SUM(total_spent) >= 58000 THEN 'Silver'
-        ELSE 'Bronze'
-    END AS customer_segment
-FROM retail_store_sales_clean
-GROUP BY customer_id
-ORDER BY lifetime_value DESC;
+    sum(total_spent) as lifetime_value,
+    case 
+        when sum(total_spent) >= 62000 then 'Gold'
+        when sum(total_spent) >= 58000 then 'Silver'
+        else 'Bronze'
+    end as customer_segment
+from retail_store_sales_clean
+group by customer_id
+order by lifetime_value desc;
 
 
 -- 9. Revenue by Discount Usage
-SELECT 
+select 
     discount_applied,
-    SUM(total_spent) AS revenue,
-    COUNT(*) AS total_orders
-FROM retail_store_sales_clean
-GROUP BY discount_applied;
+    sum(total_spent) as revenue,
+    count(*) as total_orders
+from retail_store_sales_clean
+group by discount_applied;
 
 
 -- 10. Yearly Revenue
-SELECT 
+select 
     year,
-    SUM(total_spent) AS yearly_revenue
-FROM retail_store_sales_clean
-GROUP BY year
-ORDER BY year;
+    sum(total_spent) as yearly_revenue
+from retail_store_sales_clean
+group by year
+order by year;
 
 
 -- 11. Average Order Value by Year
-SELECT 
+select 
     year,
-    ROUND(SUM(total_spent) / COUNT(*), 2) AS avg_order_value
-FROM retail_store_sales_clean
-GROUP BY year
-ORDER BY year;
+    roung(sum(total_spent) / count(*), 2) as avg_order_value
+from retail_store_sales_clean
+group by year
+order by year;
